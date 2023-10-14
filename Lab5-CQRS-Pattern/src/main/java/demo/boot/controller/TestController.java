a68689be08fd8e20698ae996e0406e19d6b553d8
jbossramana <jbossramana@gmail.com>
2023-10-14 17:11:39 +0530

    commit3

---
+++ Lab5-CQRS-Pattern/src/main/java/demo/boot/controller/TestController.java
@@ -0,0 +1,45 @@
+package demo.boot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import demo.boot.event.AccountCommandService;
+import demo.boot.event.AccountQueryService;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    private final AccountCommandService commandService;
+    private final AccountQueryService queryService;
+
+    @Autowired
+    public TestController(
+        AccountCommandService commandService,
+        AccountQueryService queryService
+    ) {
+        this.commandService = commandService;
+        this.queryService = queryService;
+    }
+
+    @PostMapping("/create")
+    public void createAccount(@RequestParam String accountId) {
+        commandService.createAccount(accountId);
+    }
+
+    @PostMapping("/deposit")
+    public void deposit(
+        @RequestParam String accountId,
+        @RequestParam double amount
+    ) {
+        commandService.deposit(accountId, amount);
+    }
+
+    @GetMapping("/balance")
+    public double getBalance(@RequestParam String accountId) {
+        return queryService.getBalance(accountId);
+    }
+}
